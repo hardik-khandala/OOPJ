40e12adc36c763f1c5b8ab54c1fe441be0a4bd53
Hardik Khandala <90664350+hardik-khandala@users.noreply.github.com>
2023-01-27 21:05:58 +0530

    Create PR1.java

---
+++ PR1.java
@@ -0,0 +1,15 @@
+public class PR1 {
+    public static void main(String[] args){
+        int a = 5;
+        float b = 4.5f;
+        String c = "Hardik";
+        boolean d = true;
+        double e = 10.45;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
