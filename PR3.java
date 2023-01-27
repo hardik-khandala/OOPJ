ca5711dfdfb9158dcd9856bf823ce677c62c4ad3
Hardik Khandala <90664350+hardik-khandala@users.noreply.github.com>
2023-01-27 21:27:13 +0530

    Create PR3.java

---
+++ PR3.java
@@ -0,0 +1,8 @@
+public class PR3 {
+    public static void main(String args[]){
+      
+        //use in command prompt to take argument and print 
+        for(int i=0;i<args.length;i++)
+        System.out.println(args[i]);
+    }
+}
