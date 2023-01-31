11aa71109d226c251d36c63a8a6c38f1e1d63f51
Hardik Khandala <90664350+hardik-khandala@users.noreply.github.com>
2023-01-31 18:36:21 +0530

    Add files via upload

---
+++ PR7.java
@@ -0,0 +1,20 @@
+class PR7 {
+	public int sum(int x, int y) { 
+        return (x + y); 
+    }
+
+	public int sum(int x, int y, int z) {
+		return (x + y + z);
+	}
+
+	public double sum(double x, double y){
+		return (x + y);
+	}
+
+	public static void main(String args[]){
+		PR7 s = new PR7();
+		System.out.println(s.sum(10, 20));
+		System.out.println(s.sum(10, 20, 30));
+		System.out.println(s.sum(10.5, 20.5));
+	}
+}
